e0233ef7bfd11cd551107c8e625ba261fe2fcb40
Nathan Soulier <souliena@f203-10.e-im2ag.ujf-grenoble.fr>
2019-12-17 14:09:17 +0100

    lecture de la section table

--- elf_reader.c
+++ elf_reader.c
@@ -7,3 +7,11 @@ int read_elf_header(FILE* file, Elf32_Ehdr* header){
 
     return 0;
 }
+int read_elf_section_table(FILE* file, Elf32_Shdr* table, size_t offset, size_t nb_entries, size_t entry_size){
+    fseek(file, offset);
+    if(fread(table, entry_size, nb_entries, file) < nb_entries){
+        return -1;
+    }
+    return 0;
+}
+
