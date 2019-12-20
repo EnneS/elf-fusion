3e4e931b094f123c0ace1c0030cbefbf4fd0441a
Cleo Surville <survillc@f203-10.e-im2ag.ujf-grenoble.fr>
2019-12-20 17:36:08 +0100

    executable affichage

--- affichage_executable.c
+++ affichage_executable.c
@@ -1,18 +1,62 @@
 #include "affichage_elf.h"
+#include <string.h>
+
+void print_info() {
+    printf("Affichage de fichier elf\n"
+                    "\tUsage : \n"
+                    "\t\t ./affichage_executable [ option ] [ nom_de_fichier_elf ] [ section ]\n"
+                    "\tOption : \n"
+                    "\t\t --help : affiche les options d'affichage\n"
+                    "\t\t -h : affiche l'en tête\n"
+                    "\t\t -x : affiche le contenue d'une section\n"
+                    "\t\t -s : affiche la table des symboles\n"
+                    "\t\t -S : affiche la table des sections\n"
+                    "\t\t -r : affiche la table de réimplémentation\n");
+}
+
+void usage() {
+            printf("usage ./affichage_executable [ option ] [ nom_de_fichier_elf ] [ section ] \"./affichage_executable --help\" pour plus d'info\n");
+}
 
 int main(int argc, char ** argv) {
-    FILE * input;
 
-    Elf32_Ehdr header;
-    if(NULL == (input = fopen("Examples_loader/example1.o","r"))) {
-        printf("Erreur lors de l'ouverture du fichier\n");
-        exit(1);
+    // VERIFICATION ENTREE
+    if( argc < 3 ) {
+        if (argc == 2 && strcmp(argv[1],"--help") == 0) {
+            print_info();
+            return 0;
+        } else {
+            usage();
+            return 0;
+        }
     }
-    if(read_elf_header(input, &header)){
-        fprintf(stderr, "couldn't recognize elf header\n");
-    }
-    print_elf_header(header);
 
-    fclose(input);
+    FILE* elf = NULL;
+
+    Elf32_data elf_data;
+
+	elf = fopen(argv[2], "r");
+
+    elf_data = read_elf_data(elf);
+
+    //AFFICHAGE FONCTION OPTION
+    if( 0 == strcmp(argv[1],"-r") ) {
+        printf("relocation table");
+    } else if ( 0 == strcmp(argv[1],"-s") ) {
+        print_symbol_table(elf_data.symbol_table, elf_data.symbol_table_size, elf_data.sm_str_table);
+    } else if( 0 == strcmp(argv[1],"-S") ) {
+        print_section_header_table(elf_data.shdr_table, reverse_4(elf_data.e_header.e_shoff), reverse_2(elf_data.e_header.e_shnum), elf_data.str_table);
+    } else if( 0 == strcmp(argv[1],"-x") ) {   
+        if (argc < 4)  {
+            usage();
+            exit(1);
+        }  
+        if( atoi(argv[3]) >= reverse_2(elf_data.e_header.e_shnum) )  {
+            printf("Erreur nombre de section = %d\n",reverse_2(elf_data.e_header.e_shnum));
+            exit(1);
+        }
+        print_section_data(elf_data.shdr_table, elf_data.str_table, elf_data.sections_data, atoi(argv[3]));
+    }
+    fclose(elf);
     return 0;
 }
